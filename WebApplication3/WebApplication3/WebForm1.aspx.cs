e340a200412596711ec5cb53ec66902199ba6cc9
DESKTOP-M7089EK\AliceYeh <yulingyeh2003@yahoo.com.tw>
2022-06-12 08:51:10 +0800

    first commit

---
+++ WebApplication3/WebApplication3/WebForm1.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+
+using System.Text;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Presentation;
+using A = DocumentFormat.OpenXml.Drawing;
+using System.IO;
+
+namespace WebApplication3
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }        //https://docs.microsoft.com/en-us/answers/questions/288217/how-to-create-scatter-chart-in-pptx-using-openxml.html
+
+    }
+}
