a69eaafb250a6091c2644842e53c07b27c24a3a3
DESKTOP-M7089EK\AliceYeh <yulingyeh2003@yahoo.com.tw>
2023-07-26 06:32:33 +0800

    test

--- MVC/ChartDemo/ChartDemo/Controllers/HomeController.cs
+++ MVC/ChartDemo/ChartDemo/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DocumentFormat.OpenXml.Math;
+using Steema.TeeChart;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,6 +11,11 @@ namespace ChartDemo.Controllers
     public class HomeController : Controller
     {
         public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Index2()
         {
             return View();
         }
@@ -25,6 +32,33 @@ namespace ChartDemo.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult GetChart()
+        {
+            int width = 800, height = 600;
+            Steema.TeeChart.TChart mChart = new TChart();
+            Steema.TeeChart.Styles.Bar mBar = new Steema.TeeChart.Styles.Bar();
+            mChart.Header.Text = "TeeChart via ImageShap PNG example";
+            mChart.Series.Add(mBar);
+            mBar.FillSampleValues();
+            mBar.XValues.DateTime = true; // mChart.Axes.Bottom.Lables.Angle = 90;
+            mChart.Axes.Bottom.Increment = Steema.TeeChart.Utils.GetDateTimeStep(DateTimeSteps.OneDay);
+            System.IO.MemoryStream ms = new System.IO.MemoryStream();
+            mChart.Export.Image.JPEG.Width = width;
+            mChart.Export.Image.JPEG.Height = height;
+
+            // TODO lock
+            mChart.Export.Image.JPEG.Save(ms);
+            ms.Position = 0;
+
+            ms.Flush();
+            System.Web.Mvc.FileContentResult res = File(ms.ToArray(), "Image/PNG", "");
+            ms.Close();
+            return res;
+
+
+            
         }
     }
 }
