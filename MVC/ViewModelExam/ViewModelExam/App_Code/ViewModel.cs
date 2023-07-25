94a7887257a24fc750097844a9de1e0c8d64efd2
DESKTOP-M7089EK\AliceYeh <yulingyeh2003@yahoo.com.tw>
2023-07-26 06:41:35 +0800

    add ViewModelExam

---
+++ MVC/ViewModelExam/ViewModelExam/App_Code/ViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ViewModelExam.App_Code
+{
+    public class ViewModel
+    {
+         
+//        做一個 SOHeaderViewModel
+
+//public HEADER_ID 單號
+//CREATEUSER 開單者 LOGONID
+//UUID 唯一編號
+//APPLIER 申請者 LOGONID
+//DateTime REQUEST_DATE { get; set; }
+
+//        USERS TABLE
+//USERID 流水號
+//LOGONID 工號
+         public string HEADER_ID { get; set; }
+         public string Remark { get; set; }
+        public string CREATEUSER { get; set; }
+        public string UUID { get; set; }
+        public string APPLIER { get; set; }
+        //public DateTime REQUEST_DATE { get; set; }
+        // SELECT 
+        //CONVERT(DATE, GETDATE()) date;
+        //select 'VS001' HEADER_ID, 'BB' Remark, '10211002' CREATEUSER, '1688' UUID, '10211002' APPLIER from dual 
+    }
+}
---
+++ MVC/ViewModelExam/ViewModelExam/WebForm1.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using ViewModelExam.App_Code;
+using ViewModelExam.DAL;
+namespace ViewModelExam
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string msg = GetData().Remark;
+            Label1.Text = "Remark =" + msg;
+        }
+
+        private ViewModel GetData()
+        {
+            string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["TestDBConnectionString"].ToString();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+
+            
+                string commandText = @"select 'VS001' HEADER_ID, 'BB' Remark, '10211002' CREATEUSER, '1688' UUID, '10211002' APPLIER 
+union all
+select 'VS002' HEADER_ID, 'BBbb' Remark, '10211002' CREATEUSER, '1688' UUID, '10211002' APPLIER --from dual";
+                SqlCommand command = new SqlCommand(commandText, connection);
+                //command.Parameters.Add("@ID", SqlDbType.Int);
+                //command.Parameters["@ID"].Value = customerID;
+                using (SqlDataAdapter da = new SqlDataAdapter(command))
+                {
+                    DataTable dt = new DataTable();
+                    dt.BeginLoadData();
+                    da.Fill(dt);
+                    dt.EndLoadData();
+                    //return dt.Rows[0]["REMARK"].ToString();
+                    List<ViewModel> list = new List<ViewModel>(dt.ToList<ViewModel>());
+                    list.Where(o => !string.IsNullOrEmpty(o.HEADER_ID)).ToList();
+                    //list.ForEach(o => { if (!"".Equals(o.CREATEUSER)) { o.USERNAME = GetUserName(o.CREATEUSER); } });
+                    return list.FirstOrDefault();
+                    //return null;
+                }
+            }
+        }
+    }
+}
